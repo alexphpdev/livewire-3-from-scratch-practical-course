2db6c51f3ec11f3e14fb9e03abdb8737c61d6999
Alexandr_Illarionov <illarionovbook@gmail.com>
2024-02-27 22:13:35 +0100

    #1 Practical Start: Form 'Live-Submit' Demo; set up application

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: '172.31.0.3',
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
